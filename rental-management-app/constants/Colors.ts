e79635beddeeb158fd37015349d4d462befb9814
deepak249108 <deepakrao541@gmail.com>
2025-03-28 20:37:58 +0530

    Add rental-management-app/constants/Colors.ts 1743174477440

---
+++ rental-management-app/constants/Colors.ts
@@ -0,0 +1,36 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    textSecondary: '#687076',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
+    card: '#f8f9fa',
+    success: '#28a745',
+    warning: '#ffc107',
+    error: '#dc3545'
+  },
+  dark: {
+    text: '#ECEDEE',
+    textSecondary: '#9BA1A6',
+    background: '#151718',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+    card: '#1e1e1e',
+    success: '#28a745',
+    warning: '#ffc107',
+    error: '#dc3545'
+  },
+};
